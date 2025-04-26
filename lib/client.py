ed6acac5e1b07cceaa755fb2032c7ad29af94181
Jjpellegrino <jpellegrino@fi.uba.ar>
2025-04-26 19:34:27 -0300

    "minicambios"

--- lib/client.py
+++ lib/client.py
@@ -1,4 +1,7 @@
 import socket
 
 def run_client(args, command_type):
-    return
+    host, port = args.host, args.port
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s_socket:
+        s_socket.bind((host, port))
+    print("Client started on {}:{}".format(host, port))
--- lib/server.py
+++ lib/server.py
@@ -5,3 +5,4 @@ def run_server(args):
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s_socket:
         s_socket.bind((host, port))
         print("Server started on {}:{}".format(host, port))
+    
