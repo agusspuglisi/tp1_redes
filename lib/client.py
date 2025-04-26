992255a85b5ec892599c09e1760b3c23784e4316
Franco <frrodriguez@fi.uba.ar>
2025-04-26 20:13:24 -0300

    Agrego start-server, y multicliente (recibe mensaje)

--- lib/client.py
+++ lib/client.py
@@ -1,7 +1,16 @@
 import socket
-
-def run_client(args, command_type):
+from lib.args_parser import parse_args_client
+ 
+def run_client(args, command):
     host, port = args.host, args.port
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s_socket:
-        s_socket.bind((host, port))
-    print("Client started on {}:{}".format(host, port))
+    message = args.name
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as c_socket:
+        c_socket.sendto(message.encode(), (host, port))
+        print(f"Sent message to {host}:{port}: {message}")
+        response, _ = c_socket.recvfrom(2048)
+        print(f"Received response: {response.decode()}")
+ 
+if __name__ == "__main__":
+    args = parse_args_client("upload")
+    run_client(args)
+ 
--- lib/server.py
+++ lib/server.py
@@ -1,8 +1,17 @@
 import socket
+import threading
+
+def handle_client(message, client_address, s_socket):
+    print(f"Received message from {client_address}: {message.decode()}")
+    s_socket.sendto(b"Message received", client_address)
 
 def run_server(args):
-    host, port = args.host, args.port #args.storage, args.protocol
+    host, port = args.host, args.port
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s_socket:
         s_socket.bind((host, port))
-        print("Server started on {}:{}".format(host, port))
-    
+        print(f"Server listening on {host}:{port}")
+
+        while True:
+            message, client_address = s_socket.recvfrom(2048)
+            client_thread = threading.Thread(target=handle_client, args=(message, client_address, s_socket))
+            client_thread.start()
