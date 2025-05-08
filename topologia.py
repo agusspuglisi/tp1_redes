ba4d22e0bf2ea716c8ea26a1472bd392d91a6133
Jjpellegrino <jpellegrino@fi.uba.ar>
2025-05-07 23:24:52 -0300

    "topologia"

--- topologia.py
+++ topologia.py
@@ -3,22 +3,24 @@ from mininet.cli import CLI
 from mininet.log import setLogLevel
 from mininet.net import Mininet
 from mininet.topo import Topo
-
+from mininet.link import TCLink 
 
 class Topo(Topo):
     def build(self, **_opts):
         s1 = self.addSwitch("s1")
-        h1 = self.addHost("h1", ip="10.0.0.2/30", defaultRoute="via 10.0.0.1")
-        h2 = self.addHost("h2", ip="10.0.0.6/30", defaultRoute="via 10.0.0.5")
+        h1 = self.addHost("h1", ip="10.0.0.2")
+        h2 = self.addHost("h2", ip="10.0.0.6")
 
-        self.addLink(h2, s1, loss = 10) 
-        self.addLink(h1, s1, loss = 10) 
+        self.addLink(h1, s1, cls=TCLink, loss=10)
+        self.addLink(h2, s1, cls=TCLink)
+        
+        
         
 
 
 def run():
     topo = Topo()
-    net = Mininet(topo=topo)
+    net = Mininet(topo=topo,link=TCLink)
     net.start()
 
     CLI(net)
